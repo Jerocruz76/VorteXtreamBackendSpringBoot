0a70202aa85ab2b76ce56b9cab0cae113ff177da
samuelacevedo9307 <samuel.acevedo.dev@gmail.com>
2024-09-06 15:09:13 -0500

    first commit jero

---
+++ app/src/main/java/com/riwi/VorteXtream_BACK_END_SpringBoot/VorteXtreamBackEndSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.VorteXtream_BACK_END_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VorteXtreamBackEndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VorteXtreamBackEndSpringBootApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/riwi/VorteXtream_BACK_END_SpringBoot/VorteXtreamBackEndSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.VorteXtream_BACK_END_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VorteXtreamBackEndSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
