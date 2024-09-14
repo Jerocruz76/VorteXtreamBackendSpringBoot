e7beb6a7c6d68f3a1f9f74f58534f5137f08cb25
Niccolo-BM <nicolasbernal0821@gmail.com>
2024-09-13 21:18:40 -0500

    First Commmit nicolasb, Subcategory finally

---
+++ app/src/main/java/com/riwi/VorteXtream_BACK_END_SpringBoot/Controllers/SubCategoriesController.java
@@ -0,0 +1,78 @@
+package com.riwi.VorteXtream_BACK_END_SpringBoot.Controllers;
+
+import com.riwi.VorteXtream_BACK_END_SpringBoot.Entities.SubCategories;
+import com.riwi.VorteXtream_BACK_END_SpringBoot.Services.Impl.SubCategoriesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/subcategories")
+public class SubCategoriesController {
+
+    private final SubCategoriesService subCategoriesService;
+
+
+    @Autowired
+    public SubCategoriesController(SubCategoriesService subCategoriesService) {
+        this.subCategoriesService = subCategoriesService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<SubCategories> createSubCategories(@RequestBody SubCategories subCategories) {
+        try {
+            SubCategories createdSubCategories = subCategoriesService.create(subCategories);
+            return new ResponseEntity<>(createdSubCategories, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(null, HttpStatus.CONFLICT);  // If name allready exist
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR); // other problems
+        }
+    }
+
+    @GetMapping("/read-all")
+    public ResponseEntity<List<SubCategories>> getAllSubCategories() {
+        try {
+            List<SubCategories> subCategories = subCategoriesService.readAll();
+            return new ResponseEntity<>(subCategories, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR); // error get data
+        }
+    }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    public ResponseEntity<Void> deleteSubCategoriesById(@PathVariable Long id) {
+        try {
+            boolean isDeleted = subCategoriesService.getDeleteById(id);
+            if (isDeleted) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);  // fine delete
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);  // id not found
+            }
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);  // Subcategory not found o predefinit
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); // delete error
+        }
+    }
+
+    @DeleteMapping("/delete-by-name/{name}")
+    public ResponseEntity<Void> deleteSubCategoriesByName(@PathVariable String name) {
+        try {
+            boolean isDeleted = subCategoriesService.deleteByName(name);
+            if (isDeleted) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);  // fine delete
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);  // name not found
+            }
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);  // Subcategory not found o predefinit
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); // delete error
+        }
+    }
+}
+
