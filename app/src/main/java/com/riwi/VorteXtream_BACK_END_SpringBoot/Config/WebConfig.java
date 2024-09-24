e28cf83636d1742a77f8681cd10f339962a16da6
David Osorio <daalosx@gmail.com>
2024-09-24 16:02:55 -0500

    add localhost origin allow

--- app/src/main/java/com/riwi/VorteXtream_BACK_END_SpringBoot/Config/WebConfig.java
+++ app/src/main/java/com/riwi/VorteXtream_BACK_END_SpringBoot/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("https://vortextreaming.netlify.app/")
+                .allowedOrigins("https://vortextreaming.netlify.app/","http://localhost:5173/")
                 .allowedMethods("GET","POST","PATCH","PUT","DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
